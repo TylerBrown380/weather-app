49d7a0630e73f95731d2eb26ff05a849d1546b38
Tyler Brown <Tylerbrown380@u.boisestate.edu>
2020-12-12 15:34:04 -0700

    refactored to dereference objects

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 const url = 'http://api.weatherstack.com/current?access_key=1ae7908cf9b52ae3a833554406a21481&query=37.8267,-122.4233&units=f'
 
-
-forecast(-71.0596,42.3605, (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data)
-})
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const myArgs = process.argv[2];
+if(!myArgs) {
+    console.log('Please provide a city')
+} else {
+    geocode(myArgs, (error, {latitude, longitude, location} = {} ) => {
+        if(error) {
+            return console.log(error);
+        } 
+        forecast(longitude,latitude, (forecastError, forecastData) => {
+            if(forecastError) {
+                return console.log(error);
+            }
+    
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+}
--- utils/forecast.js
+++ utils/forecast.js
@@ -2,14 +2,14 @@ const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=1ae7908cf9b52ae3a833554406a21481&query=' + encodeURIComponent(longitude) + ','+ encodeURIComponent(latitude) +'&units=f'
-    request({url: url, json: true}, (error, response) =>{
+    request({url, json: true}, (error, {body}) =>{
     if(error) {
         callback('Unable to connect to location services.');
-    } else if (response.body.error) {
+    } else if (body.error) {
         console.log('Unable to find location.');
     } else {
        // console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ', but it feels like ' + response.body.current.feelslike + ' degrees out.');
-       callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ', but it feels like ' + response.body.current.feelslike + ' degrees out.')
+       callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' +body.current.temperature + ', but it feels like ' + body.current.feelslike + ' degrees out.')
     }
 })
 }
